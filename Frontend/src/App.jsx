6b35a8c066cc7783e3dcfb47153a4d8db3be4895
santhoshSan475 <santhoshmurali13@gmail.com>
2026-06-15 11:32:57 +0530

    second Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-
+import './assets/css/Style.css'
+import Main from './component/Main'
+import Footer from './component/Footer'
+import Header from './component/Header'
 
 function App() {
 
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
